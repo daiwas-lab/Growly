804819583b771258ac8fb4f8fa965552c30a3469
daiwa <daiwas.lab@gmail.com>
2026-01-08 22:43:27 +0900

    TODOリスト機能　最低限の実装

--- Growly/App.js
+++ Growly/App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { styles } from './App.styles';
+import { useState } from 'react';
 
 export default function App() {
+  const [taskText, setTaskText] = useState('');
+  const [tasks, setTasks] = useState([]);
+  console.log(taskText);
+
+  const handleAddTask = () => {
+    const newTasks = { id: Date.now().toString(), text: taskText, completed: false };
+    setTasks([...tasks, newTasks]);
+    console.log('タスクを追加しました');
+
+    setTaskText('');
+  };
+
+  const handleDeleteTask = (id) => {
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
+    console.log('タスクを削除しました');
+  };
+
+  const handleToggleTask = (id) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+    console.log('タスクを完了しました');
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <Text>{item.text} {item.completed ? '完了' : '未完了'}</Text>
+        <View>
+          <TouchableOpacity onPress={() => handleToggleTask(item.id)}>
+            <Text>完了</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleDeleteTask(item.id)}>
+            <Text>削除</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Growly!</Text>
+      <TextInput placeholder="タスクを入力" style={styles.input} value={taskText} onChangeText={setTaskText} />
+      <TouchableOpacity style={styles.button} onPress={handleAddTask}>
+        <Text style={styles.buttonText}>追加</Text>
+      </TouchableOpacity>
+
+      <View>
+        <Text>タスク一覧</Text>
+        <FlatList
+          data={tasks}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Growly/App.styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: '#ccc',
+    marginBottom: 20,
+    padding: 10,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
